545c2fee0ac788ddc054d349a487cda934ad4804
RapSku <raphaelskuza@gmail.com>
2022-09-05 08:26:41 +0200

    removed apolloclient

--- services/books/frontend/src/index.tsx
+++ services/books/frontend/src/index.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import './index.css';
 import App from './App';
-
-const graphqlClient = new ApolloClient({
-  uri: 'http://localhost:8081/shelf',
-  cache: new InMemoryCache(),
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={graphqlClient}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
